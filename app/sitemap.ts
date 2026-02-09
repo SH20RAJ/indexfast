d30fdf3d2ef1ead5b392c8daa4538c02a85730b9
SH20RAJ <shaswatraj3@gmail.com>
2026-02-09 09:39:35 +0530

    fix: sitemap syntax

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,7 +25,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.5,
-    }
+    },
     {
       url: 'https://indexfast.com/pricing',
       lastModified: new Date(),
